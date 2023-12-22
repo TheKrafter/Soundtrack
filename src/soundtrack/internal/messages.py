a0fcae68dd39e75835952d0d2740a385fb117a78
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-12-22 16:03:11 -0600

    add messages file

---
+++ src/soundtrack/internal/messages.py
@@ -0,0 +1,9 @@
+# 
+# Soundtrack 
+# Copyright (c) 2023 Krafter Developer
+#
+
+# Messages
+noperm = '**🚫 No Permission**'
+notaudio = '**Could not upload.**\nOne or more files are not of proper media type `audio/mpeg`. Make sure you\'re uploading `.mp3` files.'
+badname = '**Could not upload.**\nThe `title` must not include the following characters: `#`, `>`, `.`, or `-`'
